8486ea8623f6f9a85421c9dfdf133bb8b2611835
Shanky Aggaral <145743570+shaggarwa-panga@users.noreply.github.com>
2024-05-20 15:57:18 +0530

    Update CI-CD Pipeline1.groovy

--- CI-CD Pipeline1.groovy
+++ CI-CD Pipeline1.groovy
@@ -1,20 +1,20 @@
 pipeline {
-    agent { label 'test_node' }
+    agent { label 'testnode' }
 
     stages {
         stage('Git cloning') {
             steps {
                 git branch: 'main',
-                url: 'https://github.com/Ravivarman16/DevOps-Capstone-project-2.git'
+                url: 'https://github.com/aggrawalshanky/DevOps-Capstone-project-2.git'
             }
         }
 
         stage ('Building a dockerimage') {
             steps {
-                sh 'docker rm -f $(docker ps -aq)'
-                sh 'docker build -t ravivarman46/testimage .'
-                sh 'docker run -d --name container1 -p 8080:80 ravivarman46/testimage'
-                sh 'docker push ravivarman46/testimage'
+                sh 'sudo docker rm -f $(docker ps -aq)'
+                sh 'sudo docker build . -t aggrawalshanky/testimage'
+                sh 'sudo docker run -d --name container1 -p 8080:80 aggrawalshanky/testimage'
+                sh 'sudo docker push aggrawalshanky/testimage'
             }
         }
     }
